e44bba00b9944de53443789a9d68ac123dedd5ae
Federico Mameli <federico_mameli@yahoo.it>
2022-10-08 16:12:56 +0200

    feat: config project
    add jest, eslint and typescript

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    "jest/globals": true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:jest/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "jest"],
+};
